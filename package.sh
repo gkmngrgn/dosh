45ea3ac388d55d638f122e70f748f41a0708c028
Gökmen Görgen <gkmngrgn@gmail.com>
2025-06-01 02:32:45 +0200

    package script, again.

---
+++ package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build project first
+uv build
+
+# find wheel path in dist folder, set the name and path
+export PYAPP_PROJECT_NAME="dosh"
+export PYAPP_PROJECT_PATH="$(find $(pwd)/dist -name "dosh*.whl" -type f | head -1)"
+
+# download pyapp source code, build it, and rename the binary
+curl https://github.com/ofek/pyapp/releases/latest/download/source.tar.gz -Lo pyapp-source.tar.gz
+tar -xzf pyapp-source.tar.gz
+mv pyapp-v* pyapp-latest
+cd pyapp-latest
+cargo build --release
+mv target/release/pyapp dosh-cli-linux && chmod +x dosh-cli-linux
