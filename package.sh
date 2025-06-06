8bece1f5032c52789276ee7d1958759445038c55
Gökmen Görgen <gkmngrgn@gmail.com>
2025-06-06 19:53:27 +0200

    enable all platforms.

--- package.sh
+++ package.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set output binary name (can be overridden by command line argument or environment variable)
+OUTPUT_BINARY="${DOSH_BINARY_NAME:-dosh}"
+
 # build project first
 uv build
 
@@ -7,11 +10,14 @@ uv build
 export PYAPP_PROJECT_NAME="dosh"
 export PYAPP_PROJECT_PATH="$(find $(pwd)/dist -name "dosh*.whl" -type f | head -1)"
 
-# download pyapp source code, build it, and rename the binary
+# download pyapp source code, build it
 curl https://github.com/ofek/pyapp/releases/latest/download/source.tar.gz -Lo pyapp-source.tar.gz
 tar -xzf pyapp-source.tar.gz
 mv pyapp-v* pyapp-latest
 cd pyapp-latest
 cargo build --release
+
+# and rename the binary
 mkdir -p ../bin
-mv target/release/pyapp ../bin/dosh-cli-linux && chmod +x ../bin/dosh-cli-linux
+mv target/release/pyapp "../bin/${OUTPUT_BINARY}"
+chmod +x "../bin/${OUTPUT_BINARY}"
