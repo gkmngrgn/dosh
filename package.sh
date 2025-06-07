186cd2983f6345ee50492c2aba2caa2210c8923c
Gökmen Görgen <gkmngrgn@gmail.com>
2025-06-08 01:25:00 +0200

    test packaging. (#66)
    * test packaging.
    
    * package script, again.
    
    * fix path.
    
    * enable all platforms.

---
+++ package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set output binary name (can be overridden by command line argument or environment variable)
+OUTPUT_BINARY="${DOSH_BINARY_NAME:-dosh}"
+
+# build project first
+uv build
+
+# find wheel path in dist folder, set the name and path
+export PYAPP_PROJECT_NAME="dosh"
+export PYAPP_PROJECT_PATH="$(find $(pwd)/dist -name "dosh*.whl" -type f | head -1)"
+
+# download pyapp source code, build it
+curl https://github.com/ofek/pyapp/releases/latest/download/source.tar.gz -Lo pyapp-source.tar.gz
+tar -xzf pyapp-source.tar.gz
+mv pyapp-v* pyapp-latest
+cd pyapp-latest
+cargo build --release
+
+# and rename the binary
+mkdir -p ../bin
+mv target/release/pyapp "../bin/${OUTPUT_BINARY}"
+chmod +x "../bin/${OUTPUT_BINARY}"
